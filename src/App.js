2d6530abe121c877a5ab20dce160b272c371cc95
NickG <59957371+girgvliani@users.noreply.github.com>
2023-06-09 15:11:50 +0400

    small css problems

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./styles.css";
 import users from "./data";
+import { FaUnlockAlt } from "react-icons/fa";
+import { IoPersonCircleSharp } from "react-icons/io5";
 
 function App() {
 	const [uname, setUname] = useState("");
@@ -11,37 +13,53 @@ function App() {
 		e.preventDefault();
 		for (var user of users) {
 			if (user.username === uname && user.password === pass) {
-				console.log("gj my man");
+				console.log("Welcome");
 			}
 		}
 	};
 
 	return (
-		<section className="section">
-			<p id="password"></p>
+		<section className="main">
+			<h1 className="log-in">log in</h1>
+			<img src="" className="img"></img>
+
+			<img src={require("./assets/Hood.png")} alt="" className="hood"></img>
 			<form className="input" onSubmit={(e) => checkcred(e)}>
-				<img src="" alt="logo"></img>
-				<input
-					type="input"
-					value={uname}
-					onChange={(e) => setUname(e.target.value)}
-					placeholder="Enter your Username"
-					className="input_box"
-					required
-				></input>
-				<img src="" alt="locklogo"></img>
-				<input
-					type="password"
-					value={pass}
-					placeholder="Enter your password"
-					onChange={(e) => setPass(e.target.value)}
-					required
-				></input>
-				<input type="checkbox" id="checkbox"></input>
-				<label for="checkbox">remember me</label>
-				<button className="input_submit" type="submit">
-					submit
-				</button>
+				<div>
+					<span className="icon_lock">
+						<FaUnlockAlt />
+					</span>
+					<input
+						type="input"
+						value={uname}
+						onChange={(e) => setUname(e.target.value)}
+						placeholder="Enter your Username"
+						className="input_box"
+						required
+					></input>
+				</div>
+				<div>
+					<span className="icons">
+						<IoPersonCircleSharp />
+					</span>
+					<input
+						type="password"
+						value={pass}
+						placeholder="Enter your password"
+						onChange={(e) => setPass(e.target.value)}
+						required
+					></input>
+				</div>
+				<div>
+					<input type="checkbox" id="checkbox"></input>
+					<label for="checkbox">remember me</label>
+				</div>
+
+				<div>
+					<button className="input_submit" type="submit">
+						submit
+					</button>
+				</div>
 			</form>
 		</section>
 	);
---
+++ src/mainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles.css";
+import users from "./data";
+
+export default function mainPage() {
+	return (
+		<div>
+			<div className="submitContainer">
+				<h2>submit flag</h2>
+				<input type="text" className="2"></input>
+			</div>
+		</div>
+	);
+}
